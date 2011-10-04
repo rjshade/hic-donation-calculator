e942de503579cca950bea4741f60e75ad5e35372
rjshade <robbie.shade@gmail.com>
2011-10-04 21:49:17 +0100

    simplified and working with 3 sliders

--- js/donation_calculator.js
+++ js/donation_calculator.js
@@ -33,53 +33,58 @@ function drawCanvasPeople( number )
         }
     }
 }
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(?:\d{3})+(?!\d))/g, ",");
+}
+
+var startingSalary = 20000;
+var endingSalary = 1000000;
+var yearsToWork = 50;
 
 // earningProfile is pairs of [year, salary]
 var earningProfile = []
-for( i = 0; i < 60; i++ )
-{
-    earningProfile[i] = [i, 100 * (0.1 * Math.random() + 1) * Math.pow(i,2)];
+
+
+function calculateEarningProfile(){
+  for( i = 0; i < yearsToWork; i++ )
+  {
+    y0 = startingSalary;
+    y1 = endingSalary;
+    x0 = 0;
+    x1 = yearsToWork;
+    val = y0 + i * (y1 - y0)/(yearsToWork);
+    noise = (0.1 * (Math.random() - 0.5)) * val;
+
+    earningProfile[i] = [i, val + noise];
+  }
 }
 
 
-var donationProfile = [[0,30], [35000,30], [150000,30], [999999999,30] ];
-
-var earningData = []
-var donationData = []
-
+var donationPercentage = 10;
+var earningData = [];
+var donationData = [];
 
 function fillEarningData( earningProfile )
 {
-    for( step = 0; step < earningProfile.length - 1; step++ )
-    {
-        var salary = earningProfile[step][1];
-        for( year = earningProfile[step][0]; year < earningProfile[step+1][0]; year++ )
-        {
-            earningData[year] = salary;
-        }
-    }
-    chart1.series[0].setData(earningData);
+  for( year = 0; year < yearsToWork; year++ )
+  {
+    var salary = earningProfile[year][1];
+    earningData[year] = salary;
+  }
+
+  chart1.series[0].setData(earningData);
 }
 
-function fillDonationData( donationProfile )
+function fillDonationData( earningData )
 {
-    year = 0;
     var donationData_graph = [];
-    for( step = 0; step < donationProfile.length - 1; step++ )
+    for( year = 0; year < yearsToWork; year++ )
     {
-        var lower_bound = donationProfile[step][0]
-        var upper_bound = donationProfile[step+1][0]
-
-        var percentage = donationProfile[step][1];
-
-        while( earningData[year] >= lower_bound && earningData[year] < upper_bound )
-        {
-            // inverted % for graph as we want to show it *above* the earnings
-            donationData_graph[year] = earningData[year] * (100-percentage)/100;
-            donationData[year] = earningData[year] * percentage/100;
-            year++;
-        }
+      // inverted % for graph as we want to show it *above* the earnings
+      donationData_graph[year] = earningData[year] * (100-donationPercentage)/100;
+      donationData[year] = earningData[year] * donationPercentage/100;
     }
+
     chart1.series[1].setData(donationData_graph);
 }
 
@@ -92,90 +97,82 @@ function calculateMoneyDonated()
         total += donationData[year]
     }
 
-    $( "#money_donated" ).html("£" + total.toFixed(0))
+    $( "#money_donated" ).html("£" + numberWithCommas(total.toFixed(0)))
     $( "#lives_saved" ).html( (total / 300).toFixed(0) )
 
-    drawCanvasPeople( (total / 300).toFixed(0) );
+    //drawCanvasPeople( (total / 300).toFixed(0) );
 }
 
 
 function updateDonationProfile()
 {
-    donationProfile[0][1] = $( "#slider_band_0" ).slider("value");
-    donationProfile[1][1] = $( "#slider_band_1" ).slider("value");
-    donationProfile[2][1] = $( "#slider_band_2" ).slider("value");
-}
- function band0_update( event, ui ) {
-            $( "#percentage_band_0" ).html( ui.value + "%" );
-
-            donationProfile[0][1] = ui.value;
-
-            fillEarningData( earningProfile );
-            fillDonationData( donationProfile );
-    calculateMoneyDonated();
-
+  donationPercentage = $( "#donation_slider" ).slider("value");
 }
 
+function startingSalarySliderUpdate( event, ui ) {
+  startingSalary = ui.value;
+  $( "#starting_salary_value" ).html("£" + numberWithCommas(ui.value) );
 
- function band1_update( event, ui ) {
-            $( "#percentage_band_1" ).html( ui.value + "%" );
+  calculateEarningProfile();
+  fillEarningData( earningProfile );
+  fillDonationData( earningData );
+  calculateMoneyDonated();
+}
+function endingSalarySliderUpdate( event, ui ) {
+  endingSalary = ui.value;
+  $( "#ending_salary_value" ).html("£" + numberWithCommas(ui.value) );
 
-            donationProfile[1][1] = ui.value;
-
-            fillEarningData( earningProfile );
-            fillDonationData( donationProfile );
-    calculateMoneyDonated();
+  calculateEarningProfile();
+  fillEarningData( earningProfile );
+  fillDonationData( earningData );
+  calculateMoneyDonated();
 }
 
+function donationSliderUpdate( event, ui ) {
 
- function band2_update( event, ui ) {
-            $( "#percentage_band_2" ).html( ui.value + "%" );
+  donationPercentage = ui.value;
 
-            donationProfile[2][1] = ui.value;
-
-            fillEarningData( earningProfile );
-            fillDonationData( donationProfile );
-    calculateMoneyDonated();
+  $( "#donation_value" ).html( ui.value + "%" );
+  
+  fillEarningData( earningProfile );
+  fillDonationData( earningData );
+  calculateMoneyDonated();
 }
 
 $(function() {
+  $( "#starting_salary_slider" ).slider({
+      range: "min",
+      value: 20,
+      min: 10000,
+      max: 10000000, 
+      //orientation: 'vertical',
+      change: startingSalarySliderUpdate,
+      slide: startingSalarySliderUpdate
+  });
+  $( "#starting_salary_value" ).html( "£" + numberWithCommas($( "#starting_salary_slider" ).slider( "value" )) );
 
-    $( "#slider_band_0" ).slider({
-        range: "min",
-        value: 30,
-        min: 10,
-        max: 100,
-        orientation: 'vertical',
-        change: band0_update,
-        slide: band0_update
-    });
-
-    $( "#slider_band_1" ).slider({
-        range: "min",
-        value: 30,
-        min: 10,
-        max: 100,
-        orientation: 'vertical',
-        change: band1_update,
-        slide: band1_update
-    });
+  $( "#ending_salary_slider" ).slider({
+      range: "min",
+      value: 50,
+      min: 10000,
+      max: 10000000, 
+      //orientation: 'vertical',
+      change: endingSalarySliderUpdate,
+      slide: endingSalarySliderUpdate
+  });
+  $( "#ending_salary_value" ).html( "£" + numberWithCommas($( "#ending_salary_slider" ).slider( "value" )) );
 
 
-    $( "#slider_band_2" ).slider({
-        range: "min",
-        value: 30,
-        min: 10,
-        max: 100,
-        orientation: 'vertical',
-        change: band2_update,
-        slide: band2_update
-
-    });
-
-    $( "#percentage_band_0" ).html( $( "#slider_band_0" ).slider( "value" ) + "%" );
-    $( "#percentage_band_1" ).html( $( "#slider_band_1" ).slider( "value" ) + "%" );
-    $( "#percentage_band_2" ).html( $( "#slider_band_2" ).slider( "value" ) + "%" );
-
+  $( "#donation_slider" ).slider({
+      range: "min",
+      value: 10,
+      min: 10,
+      max: 100,
+      //orientation: 'vertical',
+      change: donationSliderUpdate,
+      slide: donationSliderUpdate
+  });
+  $( "#donation_value" ).html( $( "#donation_slider" ).slider( "value" ) + "%" );
 
   chart1 = new Highcharts.Chart({
 
@@ -220,19 +217,20 @@ $(function() {
      series: [{
         name: 'Donations',
         data: [],
-        <!--color: '#f30064',-->
-        opacity: 0
+        color: '#f30064',
+        opacity: 0.5
      }, {
         name: 'Net income',
         data: [],
         color: '#ccff66',
-        opacity: 100
+        opacity: 0.5
      }]
   });
 
+  calculateEarningProfile();
   updateDonationProfile()
   fillEarningData( earningProfile );
-  fillDonationData( donationProfile );
+  fillDonationData( earningData );
   calculateMoneyDonated();
 });
 
